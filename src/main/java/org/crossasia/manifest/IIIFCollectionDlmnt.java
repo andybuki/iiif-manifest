2071ecb0e966d194c12b53e0f900c057295f5251
andrey.buchmann <ejvmVcej1979>
2021-10-07 11:46:48 +0200

    new collection

---
+++ src/main/java/org/crossasia/manifest/IIIFCollectionDlmnt.java
@@ -0,0 +1,81 @@
+package org.crossasia.manifest;
+
+import info.freelibrary.iiif.presentation.v3.Collection;
+import info.freelibrary.iiif.presentation.v3.properties.I18n;
+import info.freelibrary.iiif.presentation.v3.properties.Label;
+import info.freelibrary.iiif.presentation.v3.properties.Summary;
+import org.crossasia.manifest.attributes.DllmAttributes;
+import org.crossasia.manifest.json.StaticJsonCaller;
+import org.crossasia.manifest.metadata.LabelMetadata;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+import static org.crossasia.manifest.IIIFPresentationDlmnt.ORIGINAL_LANGUAGE;
+
+@SpringBootApplication
+public class IIIFCollectionDlmnt {
+
+	public static void main(String[] args) throws IOException {
+
+		String quote = "\u005c\u0022";
+		File absolutePath = new File("/data/dlmnt/fedora/manifests/");
+		//PrintStream out = new PrintStream(new FileOutputStream("src/main/resources/output.txt"));
+		File dir = new File(String.valueOf(absolutePath));
+		File[] filesInDir = dir.listFiles();
+		int counter = 1;
+		File created = new File("/data/dlmnt/fedora/results/");
+		Collection collection = new Collection("Collection", "Digital Library of Lanna Manuscripts 0-1000");
+		List<Collection.Item> items = new ArrayList<>();
+
+
+		for (File file : filesInDir) {
+			if (file.toString().contains(".json")) {
+				DllmAttributes dllmAttributes = new DllmAttributes();
+				int lentgthFiles = filesInDir.length;
+				JSONObject jsonObj = new JSONObject(new JSONTokener(new FileInputStream(file)));
+				StaticJsonCaller.staticJsonCaller(dllmAttributes, jsonObj);
+				JSONObject labelTitle = (JSONObject) jsonObj.get("label");
+				JSONArray arrayTitle = (JSONArray) labelTitle.get("none");
+				String title = (String) arrayTitle.get(0);
+				I18n i18n_title_Roman = LabelMetadata.getStringsLabel(dllmAttributes);
+				I18n i18n_title = LabelMetadata.getStringsLabelBoth(dllmAttributes);
+				I18n i18n_title_no_title_both = LabelMetadata.getStringsLabelNoTitleBoth(dllmAttributes);
+				Label label = new Label(title);
+				Label label_no_title_both = new Label(i18n_title_no_title_both);
+
+				Summary summary = new Summary(new I18n("en", "The Digital Library of Northern Thai Manuscripts (DLNTM) is designed as a resource for the study of traditional literature from this region. At present, the digital library contains images of over 4,200 manuscripts which can be searched and viewed online or freely downloaded, and to which more manuscripts will be added. Collaborating institutions are the University of Pennsylvania, Chiang Mai University, the National Library of Laos, and Staatsbibliothek zu Berlin. The project was funded by the Henry Luce Foundation and the German Federal Foreign Office.")
+				);
+
+				collection.setSummary(summary);
+				collection.setID("https://itr02.crossasia.org/fcrepo/rest/dlntm/collection0-2000");
+
+				int id = parseInt(dllmAttributes.getDocuments_id().
+						replace("https://itr02.crossasia.org/fcrepo/rest/dlntm/", "").
+						replace("/manifest", ""));
+				String manifestID = "https://itr02.crossasia.org/fcrepo/rest/dlntm/" + id + "/manifest";
+				Collection.Item manifest = new Collection.Item(Collection.Item.Type.MANIFEST, manifestID);
+				if (label != null) {
+					manifest.setLabel(label);
+				} else {
+					manifest.setLabel(label_no_title_both);
+				}
+
+				items.add(manifest);
+				collection.setItems(items);
+
+				DllmAttributes finalDllmAttributes = dllmAttributes;
+			}
+
+			PrintStream out = new PrintStream(new FileOutputStream("/data/dlmnt/fedora/results/collection.json"));
+			out.println(collection.toString());
+		}
+	}
+
+}
---
+++ src/main/java/org/crossasia/manifest/ManifestCurlSchript.java
@@ -0,0 +1,44 @@
+package org.crossasia.manifest;
+
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+
+public class ManifestCurlSchript {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter out = null;
+        try {
+            String manifests = "/data/dlmnt/fedora/manifests";
+            File dirManifests = new File(manifests);
+            File[] filesInManifests = dirManifests.listFiles();
+
+            String quote = "\u005c\u0022";
+            out = new BufferedWriter(new FileWriter("/data/dlmnt/fedora/manifests/files.sh"));
+            String cURLink = "";
+
+            for (File file : filesInManifests) {
+                String fileName = file.toString().replace(manifests+"/", "").replace(".json", "");
+                if (fileName.equals(manifests + "/files.sh")) {
+                    System.out.println("text file");
+                } else {
+                    try {
+                            cURLink = "curl -u fedoraAdmin:fedoraAdmin -i -X PUT" + " --data-binary @" + fileName + ".json " + " -H " +quote+ "Content-Type: application/json" + quote + " -H "+ quote+"Content-Disposition: attachment; filename=" +fileName+".json"+ quote + " "  + "http://b-lx0005.sbb.spk-berlin.de/fcrepo/rest/dlntm/" + fileName +"/manifest";
+
+                        //curl -u fedoraAdmin:fedoraAdmin -i -X PUT --data-binary @manifest.json -H "Content-Type: application/json" -H "Content-Disposition: attachment; filename=manifest.json" http://b-lx0005.sbb.spk-berlin.de/fcrepo/rest/japonica/1/manifest
+                        out.write(cURLink + "\r\n");
+                        }catch (Exception e) {
+                        e.printStackTrace();
+                        continue;
+                    }
+                    System.out.println(fileName );
+                }
+            }
+
+            System.out.println("conversion is done");
+        } catch (IOException e) {
+            System.out.println("Exception ");
+        } finally {
+            out.close();
+        }
+    }
+}
