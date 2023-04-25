1672e323690c651af6a915c4121694d306d85f30
b-ab107 <andrey.buchmann@sbb.spk-berlin.de>
2023-04-25 15:43:15 +0200

    added manifest variables

--- src/main/java/org/crossasia/manifest/statics/manifest/ManifestData.java
+++ src/main/java/org/crossasia/manifest/statics/manifest/ManifestData.java
@@ -1,4 +1,17 @@
 package org.crossasia.manifest.statics.manifest;
 
+import info.freelibrary.iiif.presentation.v3.properties.RequiredStatement;
+
 public class ManifestData {
+    public static final String SERVER = "https://iiif-content.crossasia.org/xasia/";
+    public static final String THUMBNAIL_PATH = "/full/150,/0/default.jpg";
+    public static final String MANIFEST_COLLECTION="turfan+";
+    public static final String LOGO_LINK= "https://crossasia.org/fileadmin/templates/img/xa1.png";
+    public static final String LANGUAGE = "de";
+    public static final String MANIFEST_RIGHTS = "https://creativecommons.org/licenses/by-nc-sa/4.0/";
+    public static final String MANIFEST_HOMEPAGE = "https://iiif.crossasia.org";
+    public static final String MANIFEST_HOMEPAGE_LABEL = "Crossasia IIIF collections";
+    public static final String MANIFEST_SUMMARY = "Die hier präsentierten Turfan Akten versammeln die im Museum für Asiatische Kunst (AKu) verwahrten 21 Aktenbände. Die Aktenbände I bis XV sowie der Aktenband XVII enthalten im Museum für Völkerkunde (erst Königliches Museum für Völkerkunde, später dann Staatliches Museum für Völkerkunde) archivierten Unterlagen zu den Hintergründen für die Aussendung, Vorbereitung, Planung, Ausführung und Abrechnung der Expeditionen sowie die Korrespondenz mit Wissenschaftlern bzw. Berichte und Anträge im Hinblick auf die Erforschung, Konservierung und Präsentation der aus Chinesisch Zentralasien mitgebrachten Objekte im damaligen Museum für Völkerkunde. Die Aktenbände XVI sowie XVIII bis XXI enthalten den Schriftwechsel, der ursprünglich beim sogenannten Turfan-Komitee (Lokalkomitee zur Erforschung Zentral-Asiens) verwahrt wurde, und dann, nach Auflösung des Komitees, in die Obhut des Museums für Völkerkunde überging.";
+    public static final String MANIFEST_REQUIRED_STATEMENT = "Images and metadata are made available by the National Library of Laos (CC BY-NC 4.0) via the Staatsbibliothek zu Berlin | CrossAsia";
+
 }
