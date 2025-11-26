addb231bd584c478c1c7624b08759fb9a345e71a
b-ab107 <Andrey.Buchmann@sbb.spk-berlin.de>
2025-11-26 17:10:04 +0100

    improved Canvas

--- src/main/java/org/crossasia/manifest/start/IIIFManifest.java
+++ src/main/java/org/crossasia/manifest/start/IIIFManifest.java
@@ -1,31 +1,10 @@
 package org.crossasia.manifest.start;
 
-import info.freelibrary.iiif.presentation.v3.*;
-import info.freelibrary.iiif.presentation.v3.properties.*;
 import info.freelibrary.iiif.presentation.v3.utils.Manifestor;
-
-import org.crossasia.manifest.attributes.KahlenAttributes;
-import org.crossasia.manifest.attributes.TapAttributes;
-
-import org.crossasia.manifest.canvas.CanvasKahlen;
-import org.crossasia.manifest.json.StaticJsonCallerTurfan;
-import org.crossasia.manifest.metadata.StaticFields;
-import org.crossasia.manifest.metadata.kahlen.LabelMetadata;
-import org.crossasia.manifest.transformation.IdTransformation;
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import static org.crossasia.manifest.metadata.MetadataMembersKahlen.metadataMembersKahlen;
 import static org.crossasia.manifest.start.FileCreator.fileCreator;
-import static org.crossasia.manifest.statics.manifest.ManifestData.MANIFESTS_FOLGER;
 import static org.crossasia.manifest.statics.manifest.ManifestData.RESULT_FOLGER;
-
 
 public class IIIFManifest {
     public static void buildManifest() throws IOException {
